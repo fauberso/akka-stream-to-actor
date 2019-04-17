d09c0a207fb525dfb839412f361034e2d6261256
Frederic Auberson <frederic@auberson.net>
2019-04-17 09:29:24 +0200

    Use AMQP Source instead of simple counter

--- src/main/java/net/auberson/akkaexample/streamtoactor/BookingActor.java
+++ src/main/java/net/auberson/akkaexample/streamtoactor/BookingActor.java
@@ -1,66 +1,74 @@
 package net.auberson.akkaexample.streamtoactor;
 
-import akka.actor.*;
-import akka.event.Logging;
-import akka.event.LoggingAdapter;
-import akka.japi.pf.DeciderBuilder;
-import net.auberson.akkaexample.streamtoactor.EventMessages.StreamFinishedMessage;
-import net.auberson.akkaexample.streamtoactor.EventMessages.StreamInitMessage;
-
-import java.time.Duration;
 import java.util.Optional;
 import java.util.Random;
 
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import akka.stream.alpakka.amqp.ReadResult;
+import net.auberson.akkaexample.streamtoactor.EventMessages.StreamFinishedMessage;
+import net.auberson.akkaexample.streamtoactor.EventMessages.StreamInitMessage;
+
 public class BookingActor extends AbstractActor {
-    // Simulate a failure in this percentage of incoming messages:
-    private static final int FAIL_PERCENT = 0;
+	// Simulate a failure in this percentage of incoming messages:
+	private static final int FAIL_PERCENT = 0;
 
-    Random rnd = new Random();
-    int messageCount = 0;
-    LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+	Random rnd = new Random();
+	int messageCount = 0;
+	LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
-    static Props props() {
-        return Props.create(BookingActor.class);
-    }
+	static Props props() {
+		return Props.create(BookingActor.class);
+	}
 
-    @Override
-    public Receive createReceive() {
-        return receiveBuilder() //
-                .match(BookingMessage.class, this::onMessage) //
-                .match(StreamInitMessage.class, this::onMessage) //
-                .match(StreamFinishedMessage.class, this::onMessage) //
-                .matchAny(this::onMessageAny).build();
-    }
+	@Override
+	public Receive createReceive() {
+		return receiveBuilder() //
+				.match(BookingMessage.class, this::onMessage) //
+				.match(ReadResult.class, this::onMessage) //
+				.match(StreamInitMessage.class, this::onMessage) //
+				.match(StreamFinishedMessage.class, this::onMessage) //
+				.matchAny(this::onMessageAny).build();
+	}
 
-    @Override
-    public void preRestart(Throwable reason, Optional<Object> message) throws Exception {
-        super.preRestart(reason, message);
-        log.warning("Actor restarting");
-        getSender().tell(EventMessages.messageProcessed(), self());
-    }
+	@Override
+	public void preRestart(Throwable reason, Optional<Object> message) throws Exception {
+		super.preRestart(reason, message);
+		log.warning("Actor restarting");
+		getSender().tell(EventMessages.messageProcessed(), self());
+	}
 
-    public void onMessage(StreamInitMessage message) {
-        log.info("Stream Initialized");
-        getSender().tell(EventMessages.messageProcessed(), self());
-    }
+	public void onMessage(StreamInitMessage message) {
+		log.info("Stream Initialized");
+		getSender().tell(EventMessages.messageProcessed(), self());
+	}
 
-    public void onMessage(StreamFinishedMessage message) {
-        log.info("Stream Finished");
-        getSender().tell(EventMessages.messageProcessed(), self());
-    }
+	public void onMessage(StreamFinishedMessage message) {
+		log.info("Stream Finished");
+		getSender().tell(EventMessages.messageProcessed(), self());
+	}
 
-    public void onMessage(BookingMessage message) {
-        if (messageCount++ >= 10 && rnd.nextInt(100) >= 100 - FAIL_PERCENT) {
-            // Introduce 5% chance of failure after the 10th message:
-            log.warning("Simulating an exception, this will happen randomly in roughly 5% of all cases");
-            throw new RuntimeException("A mysterious and unexprected error has happened, unable to process " + message);
-        }
+	public void onMessage(BookingMessage message) {
+		if (messageCount++ >= 10 && rnd.nextInt(100) >= 100 - FAIL_PERCENT) {
+			// Introduce 5% chance of failure after the 10th message:
+			log.warning("Simulating an exception, this will happen randomly in roughly 5% of all cases");
+			throw new RuntimeException("A mysterious and unexprected error has happened, unable to process " + message);
+		}
 
-        log.info("Booking Successful: " + message);
-        getSender().tell(EventMessages.messageProcessed(), self());
-    }
+		log.info("Booking Successful: " + message);
+		getSender().tell(EventMessages.messageProcessed(), self());
+	}
 
-    public void onMessageAny(Object o) {
-        log.error("onMessage unknown message: " + o.toString());
-    }
+	public void onMessage(ReadResult message) {
+		//TODO: Temporary...
+
+		log.info("Booking Successful: " + message);
+		getSender().tell(EventMessages.messageProcessed(), self());
+	}
+
+	public void onMessageAny(Object o) {
+		log.error("onMessage unknown message: " + o.toString());
+	}
 }
--- src/main/java/net/auberson/akkaexample/streamtoactor/Example.java
+++ src/main/java/net/auberson/akkaexample/streamtoactor/Example.java
@@ -1,47 +1,121 @@
 package net.auberson.akkaexample.streamtoactor;
 
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletionStage;
+
+import org.apache.qpid.server.SystemLauncher;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import akka.Done;
 import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import akka.actor.SupervisorStrategy;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
-import akka.japi.function.Function;
-import akka.stream.*;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import akka.stream.alpakka.amqp.AmqpCachedConnectionProvider;
+import akka.stream.alpakka.amqp.AmqpConnectionProvider;
+import akka.stream.alpakka.amqp.AmqpDetailsConnectionProvider;
+import akka.stream.alpakka.amqp.AmqpWriteSettings;
+import akka.stream.alpakka.amqp.NamedQueueSourceSettings;
+import akka.stream.alpakka.amqp.QueueDeclaration;
+import akka.stream.alpakka.amqp.ReadResult;
+import akka.stream.alpakka.amqp.javadsl.AmqpSink;
+import akka.stream.alpakka.amqp.javadsl.AmqpSource;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
+import akka.util.ByteString;
 
 public class Example implements Runnable {
 
 	private final Config config;
 	private final ActorSystem system;
+	final Materializer materializer;
 	private final LoggingAdapter log;
+	private final AmqpConnectionProvider connectionProvider;
 
-	private Example() {
+	private static final String qname = "TestQueue#" + Long.toHexString(System.currentTimeMillis()).toUpperCase();
+	private final QueueDeclaration queueDeclaration;
+
+	private Example() throws Exception {
 		config = ConfigFactory.load();
 		system = ActorSystem.create("StreamToActorExample", config);
 		log = Logging.getLogger(system, this);
+		materializer = ActorMaterializer.create(system);
+
 		log.info("Actor system started");
+
+		// Start AMQP Broker
+		startBroker();
+
+		// Initialize Alpakka connector
+		AmqpDetailsConnectionProvider amqpConnection = AmqpDetailsConnectionProvider.create("localhost", 5672);
+		connectionProvider = AmqpCachedConnectionProvider.create(amqpConnection);
+		queueDeclaration = QueueDeclaration.create(qname);
+
+		// Fill AMQP Broker with test data
+		populateBroker(qname);
+	}
+
+	/**
+	 * Starts an in-memory AMQP broker
+	 */
+	private SystemLauncher startBroker() throws Exception {
+		SystemLauncher broker = new SystemLauncher();
+		URL qpidConfigFile = this.getClass().getClassLoader().getResource("qpid-config.json");
+		log.info("Starting AMQP Broker with configuration at " + qpidConfigFile.toString());
+		Map<String, Object> attributes = new HashMap<>();
+		attributes.put("type", "Memory");
+		attributes.put("initialConfigurationLocation", qpidConfigFile.toExternalForm());
+		broker.startup(attributes);
+		log.info("AMQP Broker started.");
+
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			@Override
+			public void run() {
+				broker.shutdown();
+			}
+		}));
+
+		return broker;
+	}
+
+	private void populateBroker(String queueName) {
+		final Sink<ByteString, CompletionStage<Done>> amqpSink = AmqpSink.createSimple(AmqpWriteSettings
+				.create(connectionProvider).withRoutingKey(queueName).withDeclaration(queueDeclaration));
+
+		CompletionStage<Done> writing = Source.range(0, 999).map(i -> ByteString.fromString(Integer.toString(i)))
+				.runWith(amqpSink, materializer);
+
+		log.info("Wrote 1000 messages to " + queueName);
 	}
 
 	public void run() {
+
 		// A Regular Actor:
 		ActorRef bookingActor = system.actorOf(BookingActor.props(), "bookingActor");
 
-		// Our stream, starting with a custom source that counts from 0 to 999:
-		final Source<Integer, NotUsed> source = Source.fromGraph(new CustomSource());
+		// Our stream, starting with a custom source that counts from 0 to 999...
+		final Integer bufferSize = 1000;
+		final Source<ReadResult, NotUsed> source = AmqpSource.atMostOnceSource(NamedQueueSourceSettings
+				.create(connectionProvider, qname).withDeclaration(queueDeclaration).withAckRequired(false),
+				bufferSize);
 
-		final Sink<BookingMessage, NotUsed> sink = Sink.actorRefWithAck(bookingActor, EventMessages.streamInit(), EventMessages.messageProcessed(), EventMessages.streamFinished(), ex -> ex);
-
-		final Materializer materializer = ActorMaterializer.create(system);
-
-		//Run the stream with our actor as the sink
-		source.map(id -> new BookingMessage(id)).log("emitted").runWith(sink, materializer);
+		// ...and finishing with a Sink that acknowledges Alpakka messages:
+		// final Sink<ReadResult, NotUsed> sink =
+		// AMQPMediatorActor.getSink(mediatorActor) ;
+		final Sink<ReadResult, NotUsed> sink = Sink.actorRefWithAck(bookingActor, EventMessages.streamInit(), EventMessages.messageProcessed(), EventMessages.streamFinished(), ex -> ex);
+		
+		// Run the stream with our actor as the sink
+		source.log("emitted").runWith(sink, materializer);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		new Thread(new Example()).start();
 	}
 
